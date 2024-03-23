533780d70144261b784316b7ae1c414607075431
Majid <majid.mjh@gmail.com>
2024-03-24 00:23:45 +0100

    add unit test: Update_UpdatePlayer_ShouldReturnUpdatedPlayer

--- Rhenus/Tests/Repository.Tests/InMemoryPlayerRepositoryTests.cs
+++ Rhenus/Tests/Repository.Tests/InMemoryPlayerRepositoryTests.cs
@@ -21,4 +21,24 @@ public class InMemoryPlayerRepositoryTests
         sut.GetBy(player.Username).Should().Be(player);
         sut.GetBy(player.Id).Should().Be(player);
     }
+
+    [Fact]
+    public void Update_UpdatePlayer_ShouldReturnUpdatedPlayer()
+    {
+        // Given
+        var playerId = Guid.NewGuid();
+        var player = Player.Create(new PlayerId(playerId), "USERNAME", 10000);
+        var sut = new InMemoryPlayerRepository();
+        sut.Add(player);
+        var updatedUser = Player.Create(new PlayerId(playerId), "USERNAME", 11000);
+        
+        Console.WriteLine(player.Id.Value);
+        Console.WriteLine(updatedUser.Id.Value);
+
+        // When
+        sut.Update(updatedUser);
+
+        // Then
+        sut.GetBy(player.Id).Should().Be(updatedUser);
+    }
 }
