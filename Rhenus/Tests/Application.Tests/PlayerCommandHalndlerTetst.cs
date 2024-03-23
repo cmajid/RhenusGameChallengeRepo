20fdfd1b585a455cf7fd80575e0ce14dae974572
Majid <majid.mjh@gmail.com>
2024-03-23 15:28:53 +0100

    add integration test: Handle_ShouldHandle_PlaceBetCommand

--- Rhenus/Tests/Application.Tests/PlayerCommandHalndlerTetst.cs
+++ Rhenus/Tests/Application.Tests/PlayerCommandHalndlerTetst.cs
@@ -24,4 +24,24 @@ public class PlayerCommandHalndlerTetst
         // Then
         playerRepository.Received(1).Add(Arg.Any<Player>());
     }
+
+    [Fact]
+    public void Handle_ShouldHandle_PlaceBetCommand()
+    {
+        // Given
+        var betNumberGenerator = Substitute.For<IBetNumberGenerator>();
+        var playerId = Guid.NewGuid();
+        var player = Player.Create(new PlayerId(playerId), "USERNAME", 10000);
+        var playerRepository = Substitute.For<IPlayerRepository>();
+        playerRepository.GetBy(player.Id).Returns(player);
+        var sut = new PlayerCommandHandler(playerRepository, betNumberGenerator);
+        var command = new PlaceBetCommand(1, 100, playerId);
+
+        // When
+        sut.Handle(command);
+
+        // Then
+        playerRepository.Received(1).GetBy(player.Id);
+        playerRepository.Received(1).Update(player);
+    }
 }
